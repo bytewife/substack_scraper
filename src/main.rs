765e0a7cd9900dd5ee715b5e0c631819457a4aeb
Ivy <ivy.ember.raine@gmail.com>
2022-12-25 17:52:02 -0800

    feat: add website CLI parsing

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,30 @@ use reqwest;
 use tokio::{macros, spawn};
 use futures::executor::block_on;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+
+    /// A space-delimited list of substack sites to scrape, such as "https://blog.bytebytego.com/ https://astralcodexten.substack.com/"
+    #[clap(short, long, use_value_delimiter = true, value_delimiter = ' ')]
+    websites: Vec<String>,
+}
+
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
+    let cli = Cli::parse();
+    println!("{:?}", cli.websites);
+
     println!("---Beginning scraper---");
     let join_handle = tokio::spawn(async move {
         // Process each socket concurrently.
         scrape().await;
     });
-    // Wait for the async tas to complete.
+    // Wait for the async functions to complete.
     join_handle.await.unwrap()
 }
 
