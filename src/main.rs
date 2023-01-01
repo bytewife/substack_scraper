17f75c7b119420ea561ad7ca7988f2b0d7242501
Ivy <ivy.ember.raine@gmail.com>
2023-01-01 04:01:24 -0800

    feat: get blog posts' text content & cleanup html

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use std::collections::HashSet;
+#![feature(file_create_new)]
+
 use reqwest;
 
 use serde::{Deserialize, Serialize};
+use voca_rs::strip::strip_tags;
 use tokio::{macros, spawn};
 use futures::executor::block_on;
 use log::{debug, LevelFilter};
 use env_logger::{Builder, Target};
 use std::{env, iter};
+use std::collections::HashSet;
 use std::io::Write;
+use std::path::Path;
 use chrono::Local;
 use scraper::{Html, Selector};
 
@@ -17,6 +21,7 @@ use color_eyre::eyre;
 use color_eyre::eyre::eyre;
 use env_logger::Target::Stdout;
 use futures::TryFutureExt;
+use fancy_regex::Regex;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -51,7 +56,7 @@ async fn main() -> eyre::Result<()> {
         // TODO flip these loops lmao
     //     let join_handle = tokio::spawn(async move {
     //         // Process each socket concurrently.
-            scrape(website).await?;
+            scrape(&website).await?;
         // });
 
         // Wait for the async functions to complete.
@@ -66,14 +71,80 @@ struct CanonicalUrl {
     canonical_url: String,
 }
 
-async fn scrape(homepage_url: String) -> eyre::Result<()> {
-    let urls = get_blog_urls(homepage_url).await?;
-    debug!("Urls are {:?}", urls);
+async fn scrape(homepage_url: &String) -> eyre::Result<()> {
+    let post_urls = get_post_urls(homepage_url).await?;
+
+    // Result.
+    let mut url_to_posts: Vec<(&String, Vec<String>)> = Vec::new();
+
     // Get posts' content.
+    for mut post_url in &post_urls {
+        let post = get_post_content(&post_url).await?;
+        url_to_posts.push((&post_url, post));
+        // let post = get_post_content("https://etiennefd.substack.com/p/how-to-slow-down-progress-according".to_string()).await?;
+    }
+
+    let blog_folder_name = Path::new("blogs").join(&homepage_url.replace("https://", "").replace("/", "-"));
+
+    // Check that the url filenames do not exist.
+    for (url, _) in &url_to_posts {
+        let filename = url_to_filename(url);
+        if std::path::Path::new(&blog_folder_name).join(&filename).exists() {
+            return Err(eyre!("File {} already exists. Please delete it before running this program again.", filename));
+        }
+    }
+
+    // TODO fix this.
+    // Write to files, overwriting if exists.
+    // Create folder if it doesn't exist.
+        std::fs::create_dir_all(&blog_folder_name)?;
+    for (url, post) in url_to_posts {
+        let filename = url_to_filename(&url);
+        let path = std::path::Path::new(&blog_folder_name).join(filename);
+        let mut file = std::fs::File::create_new(path)?;
+        for line in post {
+            file.write_all(line.as_bytes())?;
+        }
+    }
+
     Ok(())
 }
 
-async fn get_blog_urls(homepage_url: String) -> eyre::Result<HashSet<String>> {
+fn url_to_filename(url: &String) -> String {
+    return url.to_owned() + ".txt";
+}
+
+/// Get the text content of a post.
+async fn get_post_content(url: &String) -> eyre::Result<Vec<String>> {
+    // TODO wait & retry getting content when hitting rate limit.
+    println!("url is {:?}", url);
+    let body = reqwest::get(url).await?.text().await?;
+    let fragment = Html::parse_fragment(&body);
+    // The following selector looks for <p> elements with the .available-content parent.
+    let selector = Selector::parse(".available-content p").unwrap();
+    let mut result = Vec::new();
+    for it in fragment.select(&selector) {
+        let temp = it.inner_html();
+        result.push(cleanup_content(&temp));
+    };
+    println!("{:?}", result);
+    Ok(result)
+}
+
+/// Transform HTML into clean text output.
+fn cleanup_content(input: &String) -> String {
+    // Replace in-paragraph footnote links with "". Assumes that the following regex works.
+    let regex_footnote = Regex::new(r">\d</a>").unwrap();
+    let temp = regex_footnote.replace_all(&input, "></a>").to_string();
+
+    // Strip HTML tags.
+    let temp = strip_tags(&temp);
+
+    // Replace &nbsp; with space.
+    temp.replace("&nbsp;", " ")
+}
+
+async fn get_post_urls(homepage_url: &String) -> eyre::Result<HashSet<String>> {
     debug!("Scraping {}", homepage_url);
 
     // Current page number.
@@ -85,22 +156,21 @@ async fn get_blog_urls(homepage_url: String) -> eyre::Result<HashSet<String>> {
     let mut seen_urls = HashSet::new();
 
     loop {
-        // Get content.
+        // Get content. The api url may be subject to change from Substack.
         let current_request_url = format!("{}api/v1/archive?sort=new&search=&offset={}&limit={}", homepage_url, page_offset, page_limit);
         debug!("current_request_url = {}", &current_request_url);
 
-        let page_urls = reqwest::get(&current_request_url)
+        let post_urls = reqwest::get(&current_request_url)
             .await?.
             json::<Vec<CanonicalUrl>>()
             .await?;
-        debug!("body = {:?}", &page_urls);
 
         // Add page URLs.
         // Exit on empty query.
-        if page_urls.is_empty() {
+        if post_urls.is_empty() {
             break;
         }
-        seen_urls.extend(page_urls.into_iter().map(|it| it.canonical_url));
+        seen_urls.extend(post_urls.into_iter().map(|it| it.canonical_url));
 
         page_offset += page_limit;
     }
